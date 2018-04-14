278de7eb653c972852a8ae91a04e97e073640d00
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-04-14 11:33:19 -0600

    Coompletado

--- app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/Main2Activity.java
@@ -3,10 +3,12 @@ package com.example.rafaj.fragmentapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     TextView text;
+    ImageView imgPlaneta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,7 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         text = findViewById(R.id.textId);
+        imgPlaneta = findViewById(R.id.imagenPlanet);
 
         Intent callingIntent = getIntent();
         String intentAction = callingIntent.getAction();
@@ -27,11 +30,42 @@ public class Main2Activity extends AppCompatActivity {
 
     }
 
-    private void handleReceivedText(Intent intent){
+    private void handleReceivedText(Intent intent) {
         String intentText = intent.getStringExtra(Intent.EXTRA_TEXT);
 
-        if (text != null){
+        if (text != null) {
             text.setText(intentText);
+
+            switch (text.getText().toString()) {
+                case "Sun":
+                    imgPlaneta.setImageResource(R.drawable.sol);
+                    break;
+                case "Mercury":
+                    imgPlaneta.setImageResource(R.drawable.mercurio);
+                    break;
+                case "Venus":
+                    imgPlaneta.setImageResource(R.drawable.venus);
+                    break;
+                case "Earth":
+                    imgPlaneta.setImageResource(R.drawable.tierra);
+                    break;
+                case "Mars":
+                    imgPlaneta.setImageResource(R.drawable.marte);
+                    break;
+                case "Jupiter":
+                    imgPlaneta.setImageResource(R.drawable.jupiter);
+                    break;
+                case "Saturn":
+                    imgPlaneta.setImageResource(R.drawable.saturn);
+                    break;
+                case "Uranus":
+                    imgPlaneta.setImageResource(R.drawable.urano);
+                    break;
+                case "Neptune":
+                    imgPlaneta.setImageResource(R.drawable.neptuno);
+                    break;
+            }
         }
+
     }
 }
